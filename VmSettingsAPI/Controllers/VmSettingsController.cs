5f0dee679ab88579ea85e6626698f99b58690b43
Damon German <damon.german@hyland.com>
2020-09-07 10:30:30 -0500

    Initial commit

---
+++ VmSettingsAPI/Controllers/VmSettingsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VmSettingsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VmSettingsController : ControllerBase
+    {
+        // GET: api/<VmSettingsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<VmSettingsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
